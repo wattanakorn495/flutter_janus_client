400c2ca91f913ddd8c758625ca14ffa778ff02c4
wattanakorn495 <41609635+wattanakorn495@users.noreply.github.com>
2022-07-22 13:10:51 +0700

    add video_call_list_event

--- lib/interfaces/video_call/events/video_call_list_event.dart
+++ lib/interfaces/video_call/events/video_call_list_event.dart
@@ -7,13 +7,33 @@ class VideoCallListEvent {
 
   VideoCallListEvent.fromJson(dynamic json) {
     videocall = json['videocall'];
+    result = json['result'] != null ? ListResult.fromJson(json['result']) : null;
   }
   String? videocall;
+  ListResult? result;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['videocall'] = videocall;
+    if (result != null) {
+      map['result'] = result?.toJson();
+    }
     return map;
   }
+}
+
+class ListResult {
+  ListResult({
+      this.list,});
+
+  List<String> list;
+
+  factory ListResult.fromJson(Map<String, dynamic> json) => ListResult(
+        list: List<String>.from(json["list"].map((x) => x)),
+  );
+
+  Map<String, dynamic> toJson() => {
+        "list": List<dynamic>.from(list.map((x) => x)),
+  };
 
 }
